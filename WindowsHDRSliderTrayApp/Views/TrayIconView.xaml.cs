c3174c7505963d0b8d2b814d81cbcbe6b1d51c4d
Anson Poon <ansonpoon77@gmail.com>
2025-08-24 18:15:11 -0700

    Make app focused when clicking tray icon

--- WindowsHDRSliderTrayApp/Views/TrayIconView.xaml.cs
+++ WindowsHDRSliderTrayApp/Views/TrayIconView.xaml.cs
@@ -1,11 +1,18 @@
 using CommunityToolkit.Mvvm.Input;
 using H.NotifyIcon;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using WinRT.Interop;
 
 namespace WindowsHDRSliderTrayApp.Views;
 
 public partial class TrayIconView : UserControl
 {
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
     public TrayIconView()
     {
         InitializeComponent();
@@ -19,9 +26,15 @@ public partial class TrayIconView : UserControl
             return;
 
         if (window.Visible)
+        {
             window.Hide();
+        }
         else
+        {
             window.Show();
+            var hwnd = WindowNative.GetWindowHandle(window);
+            SetForegroundWindow(hwnd);
+        }
 
         App.IsWindowVisible = window.Visible;
     }
